88e466679bee86b41ad927424db83c3512010d7b
josed <josed@DESKTOP-V1AK5HS>
2026-02-10 16:59:39 +0100

    r

---
+++ Practica08/src/Infiel.java
@@ -0,0 +1,9 @@
+
+public class Infiel {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- Practica08/src/ejercicios/Ej7.java
+++ Practica08/src/ejercicios/Ej7.java
@@ -1,9 +1,30 @@
 package ejercicios;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class Ej7 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
+		Random rd = new Random();
+		
+		int [] array = new int [50];
+		
+		for (int i = 0;i<array.length;i++) {
+			
+			int aleatorio = rd.nextInt(100) + 1;
+			
+			for (int j = 0;j<i;j++) {
+				
+				
+				
+			}
+		
+		}
+		
+		System.out.println(Arrays.toString(array));
+		
 
 	}
 
